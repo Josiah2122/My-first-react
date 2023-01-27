4ded03868d5fbd537996cffae49a3861f81788ca
Josiah2122 <98037818+Josiah2122@users.noreply.github.com>
2023-01-27 10:10:41 +0300

    Create Footer.js

---
+++ Footer.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default function Footer(){
+ return(
+  <footer className="my-footer"> <small>&copy; 2023 Josiah development. All rights reserved</small> </footer>
+ )
+}
