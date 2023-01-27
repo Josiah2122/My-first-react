22cac98a38cba094092d1a51df98c1a2b2a8373a
Josiah2122 <98037818+Josiah2122@users.noreply.github.com>
2023-01-27 10:12:44 +0300

    Create Body.js

---
+++ Body.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Body(){
+ return(
+  <div>
+   <h1 className="my-title">Why I'm Excited to learn React</h1>
+    <ol className="my-list">
+     <li>To make react Websites</li>
+     <li>To make money while doing it</li>
+     <li>To make beautiful, Dynamic, and reactive websites</li>
+    </ol>
+  </div>
+ )
+}
